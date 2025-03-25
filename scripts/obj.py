eef59370925e557ce401c82126c40a955ea9c108
skopek <nicolasskopek10@hotmail.com>
2025-03-25 14:11:46 -0300

    animation function added

--- scripts/obj.py
+++ scripts/obj.py
@@ -2,10 +2,22 @@ import pygame
 
 class Obj(pygame.sprite.Sprite):
 
-    def __init__(self, img, x, y, *groups):
+    def __init__(self, img, pos, *groups):
         super().__init__(*groups)
 
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+        self.frame = 0
+        self.tick = 0
+
+    def animation(self, speed, frames, path, file_type):
+        self.tick += 1
+        if self.tick > speed:
+            self.tick = 0
+            self.frame += 1
+            if self.frame > frames - 1:
+                self.frame = 0
+            self.image = pygame.image.load(path + str(self.frame) + "." + file_type)
