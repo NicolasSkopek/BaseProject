e560007da19d375497a2212112676a6928541427
skopek <nicolasskopek10@hotmail.com>
2025-03-26 18:00:54 -0300

    button class added

---
+++ scripts/button.py
@@ -0,0 +1,25 @@
+import pygame
+
+from scripts.text import Text
+
+
+class Button:
+
+    def __init__(self, color, x, y, rect_width, rect_height):
+        self.window = pygame.display.get_surface()
+        self.color = color
+        self.rect = pygame.Rect(x, y, rect_width, rect_height)
+
+
+    def draw(self):
+        pygame.draw.rect(self.window, self.color, self.rect)
+
+class ButtonWithText(Button):
+    def __init__(self, color, x, y, rect_width, rect_height, font, size, text_color, text):
+        super().__init__(color, x, y, rect_width, rect_height)
+
+        self.render = Text(font, size, text, text_color, [x, y])
+
+    def draw(self):
+        super().draw()
+        self.render.draw()
