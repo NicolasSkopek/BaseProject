05c8cf1ad5265f2f3b6cea1c7376abd46a85ad3c
skopek <nicolasskopek10@hotmail.com>
2025-03-26 18:07:34 -0300

    button's parameters fixed

--- scripts/button.py
+++ scripts/button.py
@@ -18,7 +18,11 @@ class ButtonWithText(Button):
     def __init__(self, color, x, y, rect_width, rect_height, font, size, text, text_color, text_position):
         super().__init__(color, x, y, rect_width, rect_height)
 
-        self.render = Text(font, size, text, text_color, text_position)
+        self.text = text
+        self.text_color = text_color
+        self.text_position = text_position
+
+        self.render = Text(font, size, self.text, self.text_color, self.text_position)
 
     def draw(self):
         super().draw()
